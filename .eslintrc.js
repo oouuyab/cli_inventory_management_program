9dd6a043ccbf56f8352cd40df6ca5e129849b54b
oouuyab <01073785645@naver.com>
2020-10-28 12:05:14 +0900

    :tada: Initial commit
    라이브러리 설치: commander, inquirer, eslint, eslint-config-prettier, moccha, concat-stram

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true
+    },
+    plugins: 'prettier',
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {}
+};
